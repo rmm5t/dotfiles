44d22c71cfa26bc9f627f0236c34eeadb041d6ba
Ryan McGeary <ryanongit@mcgeary.org>
2008-05-29 16:33:36 -0700

    override default git idiff

--- emacs.d/rmm-git.el
+++ emacs.d/rmm-git.el
@@ -12,4 +12,18 @@
 (autoload 'git-blame-mode "git-blame"
   "Minor mode for incremental blame for Git." t)
 
+;; Override git's ediff feature.  The default puts the buffers in the wrong
+;; order.
+;; TODO: Cleanup the temporary "HEAD" buffer after quiting ediff
+(defun git-diff-file-idiff ()
+  "Perform an interactive diff on the current file."
+  (interactive)
+  (let ((files (git-marked-files-state 'added 'deleted 'modified)))
+    (unless (eq 1 (length files))
+      (error "Cannot perform an interactive diff on multiple files."))
+    (let* ((filename (car (git-get-filenames files)))
+           (buff1 (find-file-noselect filename))
+           (buff2 (git-run-command-buffer (concat filename ".~HEAD~") "cat-file" "blob" (concat "HEAD:" filename))))
+      (ediff-buffers buff2 buff1))))
+
 (provide 'rmm-git)
