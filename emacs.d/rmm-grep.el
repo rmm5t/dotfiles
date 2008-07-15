1d5829eb844c43a78df9619f60ee01ac117e7be4
Ryan McGeary <ryanongit@mcgeary.org>
2008-07-14 20:05:49 -0400

    Ignoring .git on grep-find

--- emacs.d/rmm-grep.el
+++ emacs.d/rmm-grep.el
@@ -3,7 +3,7 @@
 ;; Grep/Find.  This needs some cleanup
 (setq grep-command "grep -Irine ")
 (setq grep-find-command
-      (format "%s . -type f \\( -name '*~' -o -name '*#' -o -name '*.log' -o -path '*CVS/*' -o -path '*.svn/*' -o -path '*vendor/*' -o -path '*build/*' -prune -o -print0 \\) | xargs -0 %s"
+      (format "%s . -type f \\( -name '*~' -o -name '*#' -o -name '*.log' -o -path '*CVS/*' -o -path '*.svn/*' -o -path '*.git/*' -o -path '*vendor/*' -o -path '*build/*' -prune -o -print0 \\) | xargs -0 %s"
               find-program grep-command))
 
 (provide 'rmm-grep)
