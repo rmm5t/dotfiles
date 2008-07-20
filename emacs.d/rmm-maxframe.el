9e90726dfad88dcb462e5acf1f11b19367494921
Ryan McGeary <ryanongit@mcgeary.org>
2008-07-20 14:04:41 -0400

    swiched to fullscreen frame parameter instead of max-frame stuff

--- emacs.d/rmm-maxframe.el
+++ emacs.d/rmm-maxframe.el
@@ -1,24 +1,29 @@
 ;;; Frame size manipulation.
 
-;; If on a Mac that supports it, go for it
-(defun rmm-maxframe-mac ()
-  (add-hook 'window-setup-hook 'mac-toggle-full-frame t)
-  (global-set-key [(f15)] 'mac-toggle-full-frame)
-  (defalias 'mf 'mac-toggle-full-frame))
+(set-frame-parameter nil 'fullscreen 'fullboth)
+;;   (set-frame-parameter nil 'fullscreen 'fullheight)
+;;   (set-frame-parameter nil 'fullscreen 'fullwidth)
+;;   (set-frame-parameter nil 'fullscreen nil)
 
-(defun rmm-maxframe-generic ()
-  (require 'maxframe)
-  ;; Biggest monitor width I currently have.  This is necessary when multiple
-  ;; monitors exist; otherwise, the frame will span multiple displays.
-  (setq mf-max-width 1920)
-  ;; Maximize on startup
-  (add-hook 'window-setup-hook 'maximize-frame t)
-  ;; Key bindings
-  (global-set-key [(f14)] 'restore-frame)
-  (global-set-key [(f15)] 'maximize-frame))
+;; ;; If on a Mac that supports it, go for it
+;; (defun rmm-maxframe-mac ()
+;;   (add-hook 'window-setup-hook 'mac-toggle-full-frame t)
+;;   (global-set-key [(f15)] 'mac-toggle-full-frame)
+;;   (defalias 'mf 'mac-toggle-full-frame))
 
-(if (fboundp 'mac-toggle-full-frame)
-    (rmm-maxframe-mac)
-  (rmm-maxframe-generic))
+;; (defun rmm-maxframe-generic ()
+;;   (require 'maxframe)
+;;   ;; Biggest monitor width I currently have.  This is necessary when multiple
+;;   ;; monitors exist; otherwise, the frame will span multiple displays.
+;;   (setq mf-max-width 1920)
+;;   ;; Maximize on startup
+;;   (add-hook 'window-setup-hook 'maximize-frame t)
+;;   ;; Key bindings
+;;   (global-set-key [(f14)] 'restore-frame)
+;;   (global-set-key [(f15)] 'maximize-frame))
+
+;; (if (fboundp 'mac-toggle-full-frame)
+;;     (rmm-maxframe-mac)
+;;   (rmm-maxframe-generic))
 
 (provide 'rmm-maxframe)
