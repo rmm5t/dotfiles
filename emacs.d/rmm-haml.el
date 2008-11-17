09a9df2b65e3f2c98e3262c77a2e87714d611f5d
Ryan McGeary <ryanongit@mcgeary.org>
2008-11-17 18:20:16 -0500

    Updated haml and sass emacs config

--- emacs.d/rmm-haml.el
+++ emacs.d/rmm-haml.el
@@ -3,4 +3,7 @@
 (require 'haml-mode)
 (require 'sass-mode)
 
+(add-to-list 'auto-mode-alist '("\\.haml$"  . haml-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$"  . sass-mode))
+
 (provide 'rmm-haml)
