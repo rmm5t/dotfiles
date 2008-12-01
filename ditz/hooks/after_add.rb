8d7d7375cd9b94f3a44ee03e0a9971d5ab68cdc3
Ryan McGeary <ryanongit@mcgeary.org>
2008-11-30 19:17:17 -0500

    Removed ditz hooks as I no longer use ditz

--- ditz/hooks/after_add.rb
+++
@@ -1,5 +0,0 @@
-Ditz::HookManager.on :after_add do |project, config, issues|
-  issues.each do |issue|
-    `git add #{issue.pathname}`
-  end
-end
--- ditz/hooks/after_delete.rb
+++
@@ -1,5 +0,0 @@
-Ditz::HookManager.on :after_delete do |project, config, issues|
-  issues.each do |issue|
-    `git rm #{issue.pathname}`
-  end
-end
