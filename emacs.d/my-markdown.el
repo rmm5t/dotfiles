95c1417eda0b32da87e9ec4a1057ee662d6da7b5
Ryan McGeary <ryanongit@mcgeary.org>
2009-01-28 19:41:59 -0500

    markdown-mode

---
+++ emacs.d/my-markdown.el
@@ -0,0 +1,7 @@
+;;; Markdown
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/markdown-mode"))
+(autoload 'markdown-mode "markdown-mode.el" nil t)
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
