feb41c4ec4cf9c506c80836e513688364d91acf4
Ryan McGeary <ryanongit@mcgeary.org>
2009-05-24 18:51:53 -0400

    Better rails template logic.

--- rails.d/template.rb
+++ rails.d/template.rb
@@ -18,35 +18,14 @@ def rewrite_database_yml
   run "cp config/database.yml.example config/database.yml"
 end
 
-unnecessary_files =
-  [
-   "public/index.html",
-   "public/dispatch.*",
-   "public/javascripts/*.js",
-   "public/images/rails.png",
-   "public/favicon.ico",
-  ]
-
-plugins =
-  {
-    "haml"                   => "git://github.com/nex3/haml.git",
-    "shoulda"                => "git://github.com/thoughtbot/shoulda.git",
-    "factory_girl"           => "git://github.com/thoughtbot/factory_girl.git",
-    "hoptoad_notifier"       => "git://github.com/thoughtbot/hoptoad_notifier.git",
-    "strip_attributes"       => "git://github.com/rmm5t/strip_attributes.git",
-    "dancing_with_sprockets" => "git://github.com/coderifous/dancing_with_sprockets.git",
-  }
-
-test_environment_config = <<-FILE
-config.gem "mocha"
-config.gem "fakeweb"
-require "factory_girl"
-FILE
-
 ############################################################
 
-unnecessary_files.each do |file|
-  run "rm #{file}"
+paperclip    = yes?("Add the paperclip gem?")
+clearance    = yes?("Add the clearance gem?")
+twitter_auth = yes?("Add the twitter-auth gem?") unless clearance
+
+%w(index.html dispatch.* javascripts/*.js images/rails.png favicon.ico).each do |public_file|
+  run "rm public/#{public_file}"
 end
 
 capify!
@@ -55,16 +34,24 @@ directory "~/.rails.d/template"
 
 rewrite_database_yml
 
-run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > app/javascripts/jquery.js"
+run "mkdir -p app/javascripts/vendor"
+run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > app/javascripts/vendor/jquery.js"
 
-environment test_environment_config, :env => :test
+gem "haml",                        :version => "2.0.9"
+gem "thoughtbot-hoptoad_notifier", :version => "1.1",     :lib => "hoptoad_notifier", :source  => "http://gems.github.com"
+gem "thoughtbot-pacecar",          :version => "1.1.5",   :lib => "pacecar",          :source  => "http://gems.github.com"
+gem "thoughtbot-paperclip",        :version => "2.2.9.1", :lib => "paperclip",        :source  => "http://gems.github.com" if paperclip
+gem "thoughtbot-clearance",        :version => "0.6.6",   :lib => "clearance",        :source  => "http://gems.github.com" if clearance
+gem "mbleigh-twitter-auth",        :version => "0.1.21",  :lib => "twitter-auth",     :source  => "http://gems.github.com" if twitter_auth
+
+gem "mocha",                   :env => :test
+gem "fakeweb",                 :env => :test
+gem "thoughtbot-shoulda",      :env => :test, :lib => "shoulda",      :source  => "http://gems.github.com"
+gem "thoughtbot-factory_girl", :env => :test, :lib => "factory_girl", :source  => "http://gems.github.com"
+
+plugin "strip_attributes",       :git => "git://github.com/rmm5t/strip_attributes.git"
+plugin "dancing_with_sprockets", :git => "git://github.com/coderifous/dancing_with_sprockets.git"
 
 git :init
-
-plugins.each do |name, repos|
-  plugin name, :git => repos, :submodule => true
-end
-
-git :submodule => :init
 git :add => "."
 git :commit => "-a -m 'Initial commit'"
