7ed3b0f5ac627c51dc8b7f18bf509376643391d4
Ryan McGeary <ryanongit@mcgeary.org>
2009-08-20 09:44:19 -0400

    Changes to tab completion everywhere handling

--- emacs.d/my-snippets.el
+++ emacs.d/my-snippets.el
@@ -2,7 +2,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
 (require 'yasnippet)
-(yas/initialize)
+(yas/global-mode 1)
 
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
 (yas/load-directory "~/.emacs.d/vendor/yasnippets-rails/rails-snippets")
@@ -10,7 +10,8 @@
 
 ;; Hippie expand.  Groovy vans with tie-dyes.
 (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand)
+
 ;; Replace yasnippets's TAB
 (add-hook 'yas/minor-mode-hook
           (lambda () (define-key yas/minor-mode-map
-                       (kbd "TAB") 'indent-or-expand))) ; was yas/expand
+                       [tab] 'smart-tab))) ; was yas/expand
--- emacs.d/my-tabs.el
+++ emacs.d/my-tabs.el
@@ -13,17 +13,39 @@
 (setq sh-basic-offset 2)
 (set-default 'javascript-indent-level 2)
 
-;; Tab completion everywhere with hippie expand
-(defun indent-or-expand (arg)
-  "Either indent according to mode, or expand the word preceding
-point."
-  (interactive "*P")
-  (if (and
-       (or (bobp) (= ?w (char-syntax (char-before))))
-       (or (eobp) (not (= ?w (char-syntax (char-after))))))
-      (hippie-expand arg)
-    (indent-according-to-mode)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Smart Tab
 
-(global-set-key (kbd "TAB") 'indent-or-expand)
-;; Revert minibuffer tab completion back to its default
-(define-key minibuffer-local-map (kbd "TAB") 'minibuffer-complete)
+(defvar smart-tab-using-hippie-expand t
+  "turn this on if you want to use hippie-expand completion.")
+
+(defun smart-tab (prefix)
+  "Needs `transient-mark-mode' to be on. This smart tab is
+  minibuffer compliant: it acts as usual in the minibuffer.
+
+  In all other buffers: if PREFIX is \\[universal-argument], calls
+  `smart-indent'. Else if point is at the end of a symbol,
+  expands it. Else calls `smart-indent'."
+  (interactive "P")
+  (labels ((smart-tab-must-expand (&optional prefix)
+                                  (unless (or (consp prefix)
+                                              mark-active)
+                                    (looking-at "\\_>"))))
+    (cond ((minibufferp)
+           (minibuffer-complete))
+          ((smart-tab-must-expand prefix)
+           (if smart-tab-using-hippie-expand
+               (hippie-expand nil)
+             (dabbrev-expand nil)))
+          ((smart-indent)))))
+
+(defun smart-indent ()
+  "Indents region if mark is active, or current line otherwise."
+  (interactive)
+  (if mark-active
+    (indent-region (region-beginning)
+                   (region-end))
+    (indent-for-tab-command)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-set-key [tab] 'smart-tab)
