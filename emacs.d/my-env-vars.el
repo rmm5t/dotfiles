1a0cb6e7f404ecd67b500224d842cfc3db561490
Ryan McGeary <ryanongit@mcgeary.org>
2009-08-30 18:14:48 -0400

    Manual ENV vars after switch from Carbon Emacs

--- emacs.d/my-env-vars.el
+++ emacs.d/my-env-vars.el
@@ -2,6 +2,8 @@
 
 (setq exec-path (cons "/opt/local/bin" exec-path))
 (setenv "PATH" (concat "/opt/local/bin:" (getenv "PATH")))
+(setq exec-path (cons "/usr/local/bin" exec-path))
+(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
 (setenv "gems" "/opt/local/lib/ruby/gems/1.8/gems")
 
