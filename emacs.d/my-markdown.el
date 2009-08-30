9e5b66a424cc3ede7f1326beda67f768e17dc795
Ryan McGeary <ryanongit@mcgeary.org>
2009-08-30 18:14:29 -0400

    Easier preview key binding for markdown-preview

--- emacs.d/my-markdown.el
+++ emacs.d/my-markdown.el
@@ -5,3 +5,8 @@
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
+
+(eval-after-load 'markdown-mode
+  '(progn
+     (define-key markdown-mode-map (kbd "C-c C-v") 'markdown-preview)
+     ))
