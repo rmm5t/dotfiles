39cfb54932e82e64826bca77a162df0403845831
Ryan McGeary <ryanongit@mcgeary.org>
2009-09-11 08:36:17 -0400

    Removed lingering escape chars in deploy.rb template

--- rails.d/template/config/deploy.rb
+++ rails.d/template/config/deploy.rb
@@ -4,7 +4,7 @@ set :repository,   "git@github.com:USERNAME/PROJECT.git" #FIXME
 set :branch,       "master"
 set :git_enable_submodules, 1
 set :deploy_via, :remote_cache
-# set :deploy_to,    "/u/apps/\#{application}"
+# set :deploy_to,    "/u/apps/#{application}"
 set :use_sudo,     false
 set :runner,       "deploy"
 set :admin_runner, "deploy"
@@ -14,7 +14,7 @@ server "app@SERVER.com:2222", :web, :app, :db, :primary => true  #FIXME
 after "deploy:update_code", "deploy:symlink_configs"
 after 'deploy:update_code' do
   rails_env = fetch(:rails_env, "production")
-  run "\#{release_path}/script/runner -e \#{rails_env} 'Sass::Plugin.update_stylesheets'"
+  run "#{release_path}/script/runner -e #{rails_env} 'Sass::Plugin.update_stylesheets'"
 end
 
 after "deploy", "deploy:cleanup"
@@ -41,28 +41,28 @@ namespace :deploy do
 
   task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
     run <<-CMD
-      ln -nfs \#{shared_path}/config/database.yml \#{release_path}/config/database.yml
+      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
     CMD
   end
 
   desc "Tail the Rails log for this environment"
   task :logs, :roles => :app do
     rails_env = fetch(:rails_env, "production")
-    run "tail -f \#{shared_path}/log/\#{rails_env}.log" do |channel, stream, data|
+    run "tail -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
       puts  # for an extra line break before the host name
-      puts "\#{channel[:host]}: \#{data}"
+      puts "#{channel[:host]}: #{data}"
       break if stream == :err
     end
   end
 
   desc "Restart the app server."
   task :restart, :roles => :app do
-    run "touch \#{current_path}/tmp/restart.txt"
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
 
 def campfire_deploy_message(options = { })
-  message = "[capistrano] \#{ENV['USER']} deployed branch \#{revision} to production"
+  message = "[capistrano] #{ENV['USER']} deployed branch #{revision} to production"
   message << " and ran migrations" if options[:migrations]
   campfire_bot message
 end
