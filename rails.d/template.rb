8922146771b34bb69ad2f7250f22e95cf1dbb8c9
Ryan McGeary <ryanongit@mcgeary.org>
2009-11-28 19:28:59 -0500

    gems.rubyforge.org == gemcutter.org

--- rails.d/template.rb
+++ rails.d/template.rb
@@ -45,31 +45,31 @@ run "mkdir -p app/javascripts/vendor"
 run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js > app/javascripts/vendor/jquery.js"
 
 gem "bluecloth",                        :version => "2.0.5" if bluecloth
-gem "haml",                             :version => "2.2.8",  :source  => "http://gemcutter.org"
-gem "giraffesoft-enum_field",           :version => "0.2.0",  :lib => "enum_field",       :source  => "http://gems.github.com"
-gem "justinfrench-formtastic",          :version => "0.2.4",  :lib => "formtastic",       :source  => "http://gems.github.com"
-gem "thoughtbot-hoptoad_notifier",      :version => "1.1",    :lib => "hoptoad_notifier", :source  => "http://gems.github.com"
-gem "thoughtbot-pacecar",               :version => "1.1.6",  :lib => "pacecar",          :source  => "http://gems.github.com"
-gem "thoughtbot-paperclip",             :version => "2.3.1",  :lib => "paperclip",        :source  => "http://gems.github.com" if paperclip
-gem "thoughtbot-clearance",             :version => "0.8.1",  :lib => "clearance",        :source  => "http://gems.github.com" if clearance
-gem "aasm",                             :version => "2.1.1",  :source  => "http://gemcutter.org" if aasm
-gem "chriseppstein-compass",            :version => "0.8.17", :source  => "http://gemcutter.org"
-gem "chriseppstein-compass-colors",     :version => "0.2.0",  :lib => "compass-colors",   :source  => "http://gems.github.com"
-gem "chriseppstein-compass-960-plugin", :version => "0.9.10", :lib => "ninesixty",        :source  => "http://gems.github.com"
-gem "ghazel-daemons",                   :version => "1.0.11", :lib => "daemons",          :source  => "http://gems.github.com" if delayed_job
-gem "delayed_job",                      :version => "1.8.2",  :source  => "http://gemcutter.org" if delayed_job
-gem "twitter-auth",                     :version => "0.1.22", :source  => "http://gemcutter.org" if twitter_auth
+gem "haml",                             :version => "2.2.14"
+gem "giraffesoft-enum_field",           :version => "0.2.0", :lib => "enum_field", :source  => "http://gems.github.com"
+gem "formtastic",                       :version => "0.9.0"
+gem "pacecar",                          :version => "1.1.8"
+gem "paperclip",                        :version => "2.3.1.1" if paperclip
+gem "clearance",                        :version => "0.8.3"   if clearance
+gem "aasm",                             :version => "2.1.3"   if aasm
+gem "compass",                          :version => "0.8.17"
+gem "compass-colors",                   :version => "0.3.1"
+gem "chriseppstein-compass-960-plugin", :version => "0.9.11", :lib => "ninesixty"
+gem "ghazel-daemons",                   :version => "1.0.11", :lib => "daemons",   :source  => "http://gems.github.com" if delayed_job
+gem "delayed_job",                      :version => "1.8.4"   if delayed_job
+gem "twitter-auth",                     :version => "0.1.22"  if twitter_auth
 
-# gem "mocha",                   :env => :test # rails auto requires this
-gem "redgreen",                :env => :test, :version => "1.2.2"
-gem "fakeweb",                 :env => :test, :version => "1.2.6"
-gem "thoughtbot-shoulda",      :env => :test, :version => "2.10.2", :lib => "shoulda",      :source  => "http://gems.github.com"
-gem "thoughtbot-factory_girl", :env => :test, :version => "1.2.2",  :lib => "factory_girl", :source  => "http://gems.github.com"
+# gem "mocha",        :env => :test # rails auto requires this
+gem "redgreen",     :env => :test, :version => "1.2.2"
+gem "fakeweb",      :env => :test, :version => "1.2.7"
+gem "shoulda",      :env => :test, :version => "2.10.2"
+gem "factory_girl", :env => :test, :version => "1.2.3"
 
 plugin "strip_attributes",       :git => "git://github.com/rmm5t/strip_attributes.git"
 plugin "dancing_with_sprockets", :git => "git://github.com/coderifous/dancing_with_sprockets.git"
 plugin "custom_err_message",     :git => "git://github.com/gumayunov/custom-err-msg.git"
 plugin "lovely_layouts",         :git => "git://github.com/justinfrench/lovely-layouts.git"
+plugin "hoptoad_notifier",       :git => "git://github.com/thoughtbot/hoptoad_notifier.git"
 
 # Compass config
 run "compass --rails --sass-dir app/stylesheets --css-dir public/stylesheets -r ninesixty -r compass-colors -f 960 ."
