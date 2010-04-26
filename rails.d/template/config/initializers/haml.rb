04ad5b01480cd6e8d9901713ec15461162ebdc84
Ryan McGeary <ryanongit@mcgeary.org>
2010-04-25 20:26:55 -0400

    Removed a dumb Haml configuration override that caused errors in production environments

--- rails.d/template/config/initializers/haml.rb
+++ rails.d/template/config/initializers/haml.rb
@@ -1,5 +1,1 @@
 Haml::Template.options = { :attr_wrapper => '"' }
-
-# TODO: This shouldn't be necessary when the base css imports a bunch of sass
-# partials. Figure out a better way around.  Perhaps, nothing to worry about.
-Sass::Plugin.options = { :always_update => true }
