bc7176a3aa53bebcc2cab765a52f376251c60c57
Ryan McGeary <ryanongit@mcgeary.org>
2010-09-21 10:14:06 -0400

    js2-mode config updates

--- emacs.d/my-javascript.el
+++ emacs.d/my-javascript.el
@@ -1,8 +1,11 @@
 ;;; Javscript is fun
 
+;; Standard js-mode
+(setq js-indent-level 2)
+
+;; ;; Enhanced js2-mode
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (setq js2-basic-offset 2)
-(setq js2-use-font-lock-faces t)
-
+(setq js2-bounce-indent-p t)
