b6964b8ce77487ee617ff82d56ca08cc7549012a
Ryan McGeary <ryanongit@mcgeary.org>
2010-09-30 10:59:31 -0400

    Avoiding git's autosetuprebase

--- emacs.d/rmm5t/magit.el
+++ emacs.d/rmm5t/magit.el
@@ -14,7 +14,7 @@
      (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
      (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
-     ;; (defun magit-pull ()
-     ;;   (interactive)
-     ;;   (magit-run-git-async "pull" "--rebase" "-v"))
+     (defun magit-pull ()
+       (interactive)
+       (magit-run-git-async "pull" "--rebase" "-v"))
      ))
