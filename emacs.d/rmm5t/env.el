a63b0015dd6155962f21c11db15eb98c17b51850
Ryan McGeary <ryanongit@mcgeary.org>
2010-10-14 11:59:08 -0400

    [emacs] Added npm/bin to the path

--- emacs.d/rmm5t/env.el
+++ emacs.d/rmm5t/env.el
@@ -4,4 +4,6 @@
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/texbin" exec-path))
 (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
+(setq exec-path (cons "/usr/local/share/npm/bin" exec-path))
+(setenv "PATH" (concat "/usr/local/share/npm/bin:" (getenv "PATH")))
 
