43f182db4dc21b4fdb73999941e005645862a5aa
Ryan McGeary <ryanongit@mcgeary.org>
2010-10-28 12:39:06 -0400

    [emacs] Added Application/Emacs.app/Contents/MacOS/bin to PATH for better magit support

--- emacs.d/rmm5t/env.el
+++ emacs.d/rmm5t/env.el
@@ -6,4 +6,6 @@
 (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/local/share/npm/bin" exec-path))
 (setenv "PATH" (concat "/usr/local/share/npm/bin:" (getenv "PATH")))
+(setq exec-path (cons "/Applications/Emacs.app/Contents/MacOS/bin" exec-path))
+(setenv "PATH" (concat "/Applications/Emacs.app/Contents/MacOS/bin:" (getenv "PATH")))
 
