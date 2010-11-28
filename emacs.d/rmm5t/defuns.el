eb3a166925631892a8283003d55b5b86c8ad8f96
Ryan McGeary <ryanongit@mcgeary.org>
2010-11-27 19:25:29 -0500

    [emacs] Added support for multiple autoload-functions passed to the vendor function

--- emacs.d/rmm5t/defuns.el
+++ emacs.d/rmm5t/defuns.el
@@ -3,7 +3,7 @@
 ;; For loading libraries from the vendor directory
 ;; Modified from defunkt's original version to support autoloading.
 ;; http://github.com/defunkt/emacs/blob/master/defunkt/defuns.el
-(defun vendor (library &optional autoload-function)
+(defun vendor (library &rest autoload-functions)
   (let* ((file (symbol-name library))
          (normal (concat "~/.emacs.d/vendor/" file))
          (suffix (concat normal ".el"))
@@ -14,7 +14,10 @@
      ((file-directory-p suffix) (add-to-list 'load-path suffix) (set 'found t))
      ((file-exists-p suffix)  (set 'found t)))
     (when found
-      (if autoload-function (autoload autoload-function (symbol-name library) nil t) (require library)))
+      (if autoload-functions
+          (dolist (autoload-function autoload-functions)
+            (autoload autoload-function (symbol-name library) nil t))
+        (require library)))
     (when (file-exists-p (concat personal ".el"))
       (load personal))))
 
