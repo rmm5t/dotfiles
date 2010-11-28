dcd5889934369cf615402fb943a5004b7041c325
Ryan McGeary <ryanongit@mcgeary.org>
2010-11-28 12:00:26 -0500

    [emacs] Updated cursor color

--- emacs.d/rmm5t/color-theme.el
+++ emacs.d/rmm5t/color-theme.el
@@ -13,12 +13,10 @@
 ;; pretty ugly.
 
 ;; Dark Backgrounds
-;; (color-theme-classic)
 ;; (color-theme-dark-laptop)
 ;; (color-theme-clarity)
 ;; (color-theme-goldenrod)
 ;; (color-theme-billw)
-;; (color-theme-gnome2)
 
 ;; Light Backgrounds
 ;; (color-theme-sitaramv-nt)
@@ -27,9 +25,8 @@
 ;; (color-theme-emacs-21)
 ;; (color-theme-gtk-ide)
 
-;; Cursor and region color overrides after switching to Emacs 23 on OSX
-;; (set-cursor-color "#014C88")
-(set-cursor-color "#666666")
-(blink-cursor-mode)
 (set-face-foreground 'region "white")
 (set-face-background 'region "slategray")
+
+(blink-cursor-mode)
+(set-cursor-color "orange")
