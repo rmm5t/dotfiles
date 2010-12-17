3c4f89a2a7353044f2f909461bb49ad6d1cef165
Ryan McGeary <ryanongit@mcgeary.org>
2010-12-17 09:07:38 -0500

    [emacs] Added comments for quickly changing font-sizes

--- emacs.d/rmm5t/fonts.el
+++ emacs.d/rmm5t/fonts.el
@@ -7,3 +7,14 @@
        "-apple-monaco-medium-r-normal--12-*-*-*-*-*-fontset-monaco,"
        "ascii:-apple-monaco-medium-r-normal--12-100-*-*-m-100-mac-roman"))
      )
+
+;; To change a font size globally:
+;; (set-face-attribute 'default nil :height 360)
+;; (set-face-attribute 'default nil :height 240)
+;; (set-face-attribute 'default nil :height 160)
+;; (set-face-attribute 'default nil :height 120)
+
+;; To change the font size interactively per buffer:
+;; Up:    C-x C-+, C-x C-=
+;; Down:  C-x C--
+;; Reset: C-x C-0
