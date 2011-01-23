99230453daff1ac91f21efc6738a7d5601dd4fdd
Ryan McGeary <ryanongit@mcgeary.org>
2011-01-23 18:49:59 -0500

    [emacs] Switched up my default ack bindings

--- emacs.d/rmm5t/bindings.el
+++ emacs.d/rmm5t/bindings.el
@@ -10,9 +10,9 @@
 (global-set-key [(control kp-2)] 'shrink-window)
 
 ;; Find stuff
-(global-set-key [(f2)]              'ack)
+(global-set-key [(f2)]              'ack-default-directory)
 (global-set-key [(control f2)]      'ack-same)
-(global-set-key [(control meta f2)] 'ack-default-directory)
+(global-set-key [(control meta f2)] 'ack)
 (global-set-key [(meta f2)]         'find-name-dired)
 (global-set-key [(shift f2)]        'occur)
 
