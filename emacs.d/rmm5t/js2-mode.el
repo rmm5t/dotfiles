23603f656c8c975665eb8159a419e5d0bad2a797
Ryan McGeary <ryanongit@mcgeary.org>
2011-02-24 23:03:15 -0500

    [emacs] Switched to the mooz fork of js2-mode
    https://github.com/mooz/js2-mode

--- emacs.d/rmm5t/js2-mode.el
+++ emacs.d/rmm5t/js2-mode.el
@@ -4,3 +4,8 @@
 
 (setq js2-basic-offset 2)
 (setq js2-bounce-indent-p t)
+
+;; Special improvements using the mooz fork
+;; https://github.com/mooz/js2-mode
+(setq js2-consistent-level-indent-inner-bracket-p t)
+(setq js2-use-ast-for-indentation-p t)
