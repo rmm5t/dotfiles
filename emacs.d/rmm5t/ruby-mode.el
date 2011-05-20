61f8a43149a11762f894c8730294c18da8d97102
Ryan McGeary <ryanongit@mcgeary.org>
2011-05-20 09:27:50 -0400

    [emacs] Added Capfile to ruby-mode list

--- emacs.d/rmm5t/ruby-mode.el
+++ emacs.d/rmm5t/ruby-mode.el
@@ -24,6 +24,7 @@
 
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("autotest$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("irbrc$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("sake$" . ruby-mode))
