cd468482b139c08ff27c46e5557c4b59d59feae2
Ryan McGeary <ryan@mcgeary.org>
2011-07-07 14:49:06 -0400

    [emacs] Added Guardfile to the ruby-mode list

--- emacs.d/rmm5t/ruby-mode.el
+++ emacs.d/rmm5t/ruby-mode.el
@@ -25,6 +25,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("autotest$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("irbrc$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("sake$" . ruby-mode))
