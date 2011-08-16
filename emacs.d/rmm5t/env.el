44553477df70da6936d17449748706c319d969df
Ryan McGeary <ryan@mcgeary.org>
2011-08-16 13:03:53 -0400

    [emacs] Added rbenv paths

--- emacs.d/rmm5t/env.el
+++ emacs.d/rmm5t/env.el
@@ -2,6 +2,10 @@
 
 (setq exec-path (cons "/usr/local/bin" exec-path))
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(setq exec-path (cons "~/.rbenv/bin" exec-path))
+(setenv "PATH" (concat "~/.rbenv/bin:" (getenv "PATH")))
+(setq exec-path (cons "~/.rbenv/shims" exec-path))
+(setenv "PATH" (concat "~/.rbenv/shims:" (getenv "PATH")))
 (setq exec-path (cons "/usr/texbin" exec-path))
 (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
 (setq exec-path (cons "/usr/local/share/npm/bin" exec-path))
