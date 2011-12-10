8df2ea206174beb56ae359f619e541480a1b0fd2
Ryan McGeary <ryan@mcgeary.org>
2011-12-10 15:43:41 -0500

    Improved magit-item-highlight display
    The underlines are ugly

--- emacs.d/rmm5t/magit.el
+++ emacs.d/rmm5t/magit.el
@@ -8,9 +8,9 @@
 
 (eval-after-load 'magit
   '(progn
-     ;; (set-face-background 'magit-item-highlight "gray10")
-     (set-face-background 'magit-item-highlight nil)
      (set-face-background 'highlight nil) ;; highlight is overriding other background colors for diff chunks
+     (set-face-foreground 'highlight nil) ;; highlight is overriding other foreground colors for diff chunks
+     (set-face-underline  'highlight nil)
      (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
      (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
