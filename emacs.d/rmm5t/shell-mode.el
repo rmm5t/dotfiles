0467192507bdae9e63e0d6fd886e0a17da419036
Ryan McGeary <ryan@mcgeary.org>
2012-11-28 23:42:11 -0500

    Added .powenv as match for shell mode

--- emacs.d/rmm5t/shell-mode.el
+++ emacs.d/rmm5t/shell-mode.el
@@ -5,5 +5,6 @@
 (add-to-list 'auto-mode-alist '("bash_aliases$" . sh-mode))
 (add-to-list 'auto-mode-alist '("bash_local$" . sh-mode))
 (add-to-list 'auto-mode-alist '("bash_completion$" . sh-mode))
+(add-to-list 'auto-mode-alist '(".powenv$" . sh-mode))
 
 (add-hook 'shell-mode-hook (lambda () (setq tab-width 8)))
