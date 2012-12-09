2ad59d8557dac875e6da11e50b59efe2abec3441
Ryan McGeary <ryan@mcgeary.org>
2012-12-08 21:36:59 -0500

    Fixed deprecation warning after emacs (and org-mode) upgrade

--- emacs.d/rmm5t/org.el
+++ emacs.d/rmm5t/org.el
@@ -1,6 +1,6 @@
 ;;; org-mode
 
-(require 'org-install)
+(require 'org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
