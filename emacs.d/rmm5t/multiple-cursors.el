c47a65ef82b1db78cbb303b2e4fce4347be82446
Ryan McGeary <ryan@mcgeary.org>
2013-01-31 10:42:31 -0500

    Changed multiple-cursor key bindings

--- emacs.d/rmm5t/multiple-cursors.el
+++ emacs.d/rmm5t/multiple-cursors.el
@@ -1,5 +1,5 @@
 (global-set-key (kbd "C-c C-.") 'mc/edit-lines)
 (global-set-key (kbd "C->")     'mc/mark-next-like-this)
 (global-set-key (kbd "C-<")     'mc/mark-previous-like-this)
-(global-set-key (kbd "C-M->")   'mc/mark-all-like-this)
+(global-set-key (kbd "C-s-.")   'mc/mark-all-like-this)
 (global-set-key (kbd "s-SPC")   'set-rectangular-region-anchor)
