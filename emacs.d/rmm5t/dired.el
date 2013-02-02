17a38ba0b2cbe915bdb6a4a344bcea90a3bb2c3d
Ryan McGeary <ryan@mcgeary.org>
2013-02-01 23:12:21 -0500

    Improved dired navigation

--- emacs.d/rmm5t/dired.el
+++ emacs.d/rmm5t/dired.el
@@ -2,3 +2,20 @@
 
 ;; Allows recursive deletes
 (setq dired-recursive-deletes 'top)
+
+;; Borrowed from http://whattheemacsd.com/setup-dired.el-02.html
+(defun dired-back-to-top ()
+  (interactive)
+  (beginning-of-buffer)
+  (dired-next-line 4))
+
+(define-key dired-mode-map
+  (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+
+(defun dired-jump-to-bottom ()
+  (interactive)
+  (end-of-buffer)
+  (dired-next-line -1))
+
+(define-key dired-mode-map
+  (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
