43d749aeef852a2440c142813de88d955106a6c9
Ryan McGeary <ryan@mcgeary.org>
2013-02-01 23:49:05 -0500

    Fixed dired and magit mode-map key bindings

--- emacs.d/rmm5t/dired.el
+++ emacs.d/rmm5t/dired.el
@@ -9,13 +9,15 @@
   (beginning-of-buffer)
   (dired-next-line 4))
 
-(define-key dired-mode-map
-  (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
-
 (defun dired-jump-to-bottom ()
   (interactive)
   (end-of-buffer)
   (dired-next-line -1))
 
-(define-key dired-mode-map
-  (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (define-key dired-mode-map
+              (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+            (define-key dired-mode-map
+              (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
+            ))
--- emacs.d/rmm5t/magit.el
+++ emacs.d/rmm5t/magit.el
@@ -11,17 +11,6 @@
    (setq fill-column 72)
    (turn-on-auto-fill)))
 
-(eval-after-load 'magit
-  '(progn
-     (set-face-background 'highlight nil) ;; highlight is overriding other background colors for diff chunks
-     (set-face-foreground 'highlight nil) ;; highlight is overriding other foreground colors for diff chunks
-     (set-face-underline  'highlight nil)
-     (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
-     (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
-     (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
-     (define-key magit-mode-map (kbd "<tab>") 'magit-toggle-section)    ; was smart-tab
-     ))
-
 ;; full screen magit-status
 ;; borrowed from http://whattheemacsd.com/setup-magit.el-01.html
 (defadvice magit-status (around magit-fullscreen activate)
@@ -35,4 +24,15 @@
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
 
-(define-key magit-status-mode-map (kbd "M-K") 'magit-quit-session)
+
+(eval-after-load 'magit
+  '(progn
+     (set-face-background 'highlight nil) ;; highlight is overriding other background colors for diff chunks
+     (set-face-foreground 'highlight nil) ;; highlight is overriding other foreground colors for diff chunks
+     (set-face-underline  'highlight nil)
+     (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
+     (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
+     (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
+     (define-key magit-mode-map (kbd "<tab>") 'magit-toggle-section)    ; was smart-tab
+     (define-key magit-status-mode-map (kbd "M-K") 'magit-quit-session)
+     ))
