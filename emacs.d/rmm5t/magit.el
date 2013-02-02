8fbb3342aec8538d121fd1977d9e5fbe1db3c6e4
Ryan McGeary <ryan@mcgeary.org>
2013-02-01 23:33:59 -0500

    Added full-screen magit-status

--- emacs.d/rmm5t/magit.el
+++ emacs.d/rmm5t/magit.el
@@ -21,3 +21,18 @@
      (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
      (define-key magit-mode-map (kbd "<tab>") 'magit-toggle-section)    ; was smart-tab
      ))
+
+;; full screen magit-status
+;; borrowed from http://whattheemacsd.com/setup-magit.el-01.html
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "M-K") 'magit-quit-session)
