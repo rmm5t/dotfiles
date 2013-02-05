ca15409feae7c8097cc2c72d733740f2c19f2b21
Ryan McGeary <ryan@mcgeary.org>
2013-02-05 14:31:54 -0500

    Added defun to use Marked.app as potential markdown-preview

--- emacs.d/rmm5t/markdown-mode.el
+++ emacs.d/rmm5t/markdown-mode.el
@@ -4,7 +4,20 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.text$" . gfm-mode))
 
+;; Original premise borrowed from https://gist.github.com/siancu/4542778
+(defun rmm5t-markdown-preview ()
+  "run Marked on the current file if it exists; otherwise
+fallback to markdown-preview"
+  (interactive)
+  (let ((marked-app "/Applications/Marked.app"))
+    (if (file-exists-p marked-app)
+        (shell-command
+         (format (concat "open -a " marked-app " %s")
+                 (shell-quote-argument (buffer-file-name))))
+      (markdown-preview))
+    ))
+
 (eval-after-load 'markdown-mode
   '(progn
-     (define-key markdown-mode-map (kbd "C-c C-v") 'markdown-preview)
+     (define-key markdown-mode-map (kbd "C-c C-v") 'rmm5t-markdown-preview)
      ))
