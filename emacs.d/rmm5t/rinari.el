756a527e629c84e321dc17f5b5f5b0f19f3c4b1d
Ryan McGeary <ryan@mcgeary.org>
2013-02-09 14:57:03 -0500

    Added global-rinari-mode declaration for latest version of rinari

--- emacs.d/rmm5t/rinari.el
+++ emacs.d/rmm5t/rinari.el
@@ -1,3 +1,5 @@
+(global-rinari-mode)
+
 (define-key rinari-minor-mode-map (kbd "C-c t") 'rinari-find-test)
 (define-key rinari-minor-mode-map (kbd "C-c m") 'rinari-find-model)
 (define-key rinari-minor-mode-map (kbd "C-c c") 'rinari-find-controller)
