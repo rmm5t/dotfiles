c11dc37562408d907257facafb175fc68b1a3c54
Ryan McGeary <ryan@mcgeary.org>
2014-06-16 10:53:52 -0400

    Upped emacs deps

--- emacs.d/rmm5t/kbd-macros.el
+++ emacs.d/rmm5t/kbd-macros.el
@@ -1,2 +1,5 @@
 (fset 'ruby-hash-converter
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 61 62 13 backspace backspace 134217820 58 32 18 58 18 13 4] 0 "%d")) arg)))
+
+(fset 'modevux-guests
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([1 123 32 102 105 114 115 116 95 110 97 109 101 58 33554464 34 134217830 34 44 32 108 97 115 116 95 110 97 109 101 58 134217820 32 34 134217830 34 44 32 101 109 97 105 108 58 134217820 32 34 19 32 13 134217820 34 44 32 99 111 109 112 97 110 121 58 33554464 34 5 34 32 125 14] 0 "%d")) arg)))
