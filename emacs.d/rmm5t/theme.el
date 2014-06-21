45a8ab5175ab32355666482aa38ae5c8f5aa561a
Ryan McGeary <ryan@mcgeary.org>
2014-06-21 14:39:30 -0400

    Changed cursor, region, and isearch emacs colors

--- emacs.d/rmm5t/theme.el
+++ emacs.d/rmm5t/theme.el
@@ -1,3 +1,8 @@
 ;;; Theme customization
 
 (load-theme 'wombat)
+
+(set-cursor-color "orange")
+(set-face-attribute 'region nil :background "cornflower blue")
+(set-face-attribute 'isearch nil :background "gold" :foreground "black")
+(set-face-attribute 'lazy-highlight nil :background "dark goldenrod" :foreground "white")
