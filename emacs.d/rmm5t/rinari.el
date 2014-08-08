6337b6f810cbdbe52a56d9c84c3bb7d0a7afd1f2
Ryan McGeary <ryan@mcgeary.org>
2014-08-08 15:08:55 -0400

    Added more rinari jump bindings

--- emacs.d/rmm5t/rinari.el
+++ emacs.d/rmm5t/rinari.el
@@ -4,3 +4,5 @@
 (define-key rinari-minor-mode-map (kbd "C-c m") 'rinari-find-model)
 (define-key rinari-minor-mode-map (kbd "C-c c") 'rinari-find-controller)
 (define-key rinari-minor-mode-map (kbd "C-c v") 'rinari-find-view)
+(define-key rinari-minor-mode-map (kbd "C-c i") 'rinari-find-migration)
+(define-key rinari-minor-mode-map (kbd "C-c h") 'rinari-find-helper)
