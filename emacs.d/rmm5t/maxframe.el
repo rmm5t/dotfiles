558f3ba739ebcdccdb3dc20a718e54a968ffb4dc
Ryan McGeary <ryan@mcgeary.org>
2014-11-04 11:56:39 -0700

    Ensured maximizing frame happens on window-setup-hook

--- emacs.d/rmm5t/maxframe.el
+++ emacs.d/rmm5t/maxframe.el
@@ -3,21 +3,21 @@
 (defun rmm5t-init-fullscreen ()
   ;; (setq ns-auto-hide-menu-bar t)
   (setq ns-use-native-fullscreen nil)
-  (toggle-frame-fullscreen)
-  (global-set-key [(meta return)] 'toggle-frame-fullscreen))
+  (global-set-key [(meta return)] 'toggle-frame-fullscreen)
+  (add-hook 'window-setup-hook 'toggle-frame-fullscreen t))
 
 (defun rmm5t-init-ns-fullscreen ()
   ;; (setq ns-auto-hide-menu-bar t)
   (setq ns-use-native-fullscreen nil)
-  (ns-toggle-fullscreen)
-  (global-set-key [(meta return)] 'ns-toggle-fullscreen))
+  (global-set-key [(meta return)] 'ns-toggle-fullscreen)
+  (add-hook 'window-setup-hook 'ns-toggle-fullscreen t))
 
 (defun rmm5t-init-maxframe ()
   ;; Biggest monitor width I currently have.  This is necessary when multiple
   ;; monitors exist; otherwise, the frame will span multiple displays.
   (setq mf-max-width 2560)
-  (add-hook 'window-setup-hook 'maximize-frame t)
-  (global-set-key [(meta return)] 'mf))
+  (global-set-key [(meta return)] 'mf)
+  (add-hook 'window-setup-hook 'maximize-frame t))
 
 (if (window-system)
     (cond
