a653ced5ced65ea82ea4b9fb602347fa5921312d
Ryan McGeary <ryan@mcgeary.org>
2016-01-31 19:26:29 -0700

    Tied maxframe init to after-init-hook instead of window-setup-hook
    Allows `ns-use-native-fullscreen` set to nil to work as desired, instead
    of using native OS X (spaces-based) fullscreen.

--- emacs.d/personal/maxframe.el
+++ emacs.d/personal/maxframe.el
@@ -4,20 +4,20 @@
   ;; (setq ns-auto-hide-menu-bar t)
   (setq ns-use-native-fullscreen nil)
   (global-set-key [(meta return)] 'toggle-frame-fullscreen)
-  (add-hook 'window-setup-hook 'toggle-frame-fullscreen t))
+  (add-hook 'after-init-hook 'toggle-frame-fullscreen t))
 
 (defun personal-init-ns-fullscreen ()
   ;; (setq ns-auto-hide-menu-bar t)
   (setq ns-use-native-fullscreen nil)
   (global-set-key [(meta return)] 'ns-toggle-fullscreen)
-  (add-hook 'window-setup-hook 'ns-toggle-fullscreen t))
+  (add-hook 'after-init-hook 'ns-toggle-fullscreen t))
 
 (defun personal-init-maxframe ()
   ;; Biggest monitor width I currently have.  This is necessary when multiple
   ;; monitors exist; otherwise, the frame will span multiple displays.
   (setq mf-max-width 2560)
   (global-set-key [(meta return)] 'mf)
-  (add-hook 'window-setup-hook 'maximize-frame t))
+  (add-hook 'after-init-hook 'maximize-frame t))
 
 (if (window-system)
     (cond
