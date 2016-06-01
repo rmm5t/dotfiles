e1ccf5cc5678bdcd7290b8b7f799209f4a24260c
Ryan McGeary <ryan@mcgeary.org>
2016-06-01 15:37:18 -0600

    Fixed grep-find-command for newer emacs installations

--- emacs.d/personal/grep.el
+++ emacs.d/personal/grep.el
@@ -2,6 +2,11 @@
 
 ;; Grep/Find.  This needs some cleanup
 (setq grep-command "grep -Irine ")
-(setq grep-find-command
-      (format "%s . -type f \\( -name '*~' -o -name '*#' -o -name '*.log' -o -path '*CVS/*' -o -path '*.svn/*' -o -path '*.git/*' -o -path '*vendor/*' -o -path '*build/*' -prune -o -print0 \\) | xargs -0 %s"
-              find-program grep-command))
+
+(eval-after-load 'grep
+  '(progn
+     (add-to-list 'grep-find-ignored-files "*.log")
+     (add-to-list 'grep-find-ignored-directories "log")
+     (add-to-list 'grep-find-ignored-directories "vendor")
+     (add-to-list 'grep-find-ignored-directories "build")
+     ))
