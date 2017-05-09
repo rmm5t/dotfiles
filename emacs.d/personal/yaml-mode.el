1483ebf2afb0798babf1ecef3488229ab2623652
Ryan McGeary <ryan@mcgeary.org>
2017-05-08 20:30:13 -0600

    Made .eslintrc load yaml-mode

--- emacs.d/personal/yaml-mode.el
+++ emacs.d/personal/yaml-mode.el
@@ -1,4 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.eslintrc$" . yaml-mode))
 (add-hook 'yaml-mode-hook
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
