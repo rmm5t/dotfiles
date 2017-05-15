19ef7436dd5029596063e735a594006b0b37599f
Ryan McGeary <ryan@mcgeary.org>
2017-05-14 18:39:53 -0600

    Future proofing emacs 26 compatibility

--- emacs.d/personal/utf-8.el
+++ emacs.d/personal/utf-8.el
@@ -5,5 +5,3 @@
 (set-default buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
-(set-default default-buffer-file-coding-system 'utf-8-unix)
-
