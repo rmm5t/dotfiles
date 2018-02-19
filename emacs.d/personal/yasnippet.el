ffabc33277eb1d22e8894276f1c252a2fe229dda
Ryan McGeary <ryan@mcgeary.org>
2018-02-18 18:00:47 -0700

    Remove emacs packages no longer in ELPA

--- emacs.d/personal/yasnippet.el
+++ emacs.d/personal/yasnippet.el
@@ -1,7 +1,5 @@
 ;;; Snippets make typing fun
 
-(require 'dropdown-list)
-
 (yas-global-mode 1)
 
 ;; (yas-load-directory "~/.emacs.d/vendor/yasnippet/snippets")
@@ -9,7 +7,6 @@
 ;; (yas-load-directory "~/.emacs.d/vendor/yasnippets-shoulda")
 
 (setq yas-prompt-functions '(yas-ido-prompt
-                             yas-dropdown-prompt
                              yas-completing-prompt))
 
 ;; (add-hook 'yas-minor-mode-hook
