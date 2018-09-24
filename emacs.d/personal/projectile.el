4123e3cba3937bfea90e03483812aeaed2ba18b5
Ryan McGeary <ryan@mcgeary.org>
2018-09-24 07:35:48 -0600

    Update projectile.el

--- emacs.d/personal/projectile.el
+++ emacs.d/personal/projectile.el
@@ -1,1 +1,3 @@
-(projectile-global-mode)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
