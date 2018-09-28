b546d003362d00b04c1b6928fae4770144b656d6
Ryan McGeary <ryan@mcgeary.org>
2018-09-28 15:47:25 -0600

    Remove obsolete comments in js2-mode config

--- emacs.d/personal/js2-mode.el
+++ emacs.d/personal/js2-mode.el
@@ -5,9 +5,6 @@
 (setq js2-cleanup-whitespace t)
 (setq js2-basic-offset 2)
 (setq js2-bounce-indent-p t)
-
-;; Special improvements using the mooz fork
-;; https://github.com/mooz/js2-mode
 (setq js2-consistent-level-indent-inner-bracket-p t)
 (setq js2-use-ast-for-indentation-p t)
 
