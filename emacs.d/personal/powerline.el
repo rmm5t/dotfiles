0f158e23de2edcd581e090afaa3310b1957cc70f
Ryan McGeary <ryan.mcgeary@invesco.com>
2021-08-17 08:38:38 -0600

    Fix powerline colors for macos

--- emacs.d/personal/powerline.el
+++ emacs.d/personal/powerline.el
@@ -2,6 +2,8 @@
 
 (powerline-default-theme)
 
+(setq powerline-image-apple-rgb nil)
+
 (custom-set-faces
  '(mode-line         ((t (:background "#ffcc00" :foreground "black"))))
  '(powerline-active1 ((t (:background "grey22" :foreground "white" :inherit mode-line))))
