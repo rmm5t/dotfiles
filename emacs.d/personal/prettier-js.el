1c3baeaef20fb4685a9ee303b0233f62718e4892
Ryan McGeary <ryan@mcgeary.org>
2026-01-01 10:52:05 -0700

    Fix prettier-js config

--- emacs.d/personal/prettier-js.el
+++ emacs.d/personal/prettier-js.el
@@ -1,7 +1,4 @@
-(require 'prettier-js)
+(use-package prettier-js)
 
-(eval-after-load 'js-mode
-  '(add-hook 'js-mode-hook #'prettier-js-mode))
-
-(eval-after-load 'js2-mode
-  '(add-hook 'js2-mode-hook #'prettier-js-mode))
+(add-hook 'js-mode-hook 'prettier-js-mode)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
