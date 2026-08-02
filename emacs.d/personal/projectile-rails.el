4c579405fb8f3bd8c4d73d0a9865dd2f177ae541
Ryan McGeary <ryan@mcgeary.org>
2026-08-01 18:58:30 -0600

    projectile rails key

--- emacs.d/personal/projectile-rails.el
+++ emacs.d/personal/projectile-rails.el
@@ -3,3 +3,5 @@
 (setq projectile-rails-vanilla-command "bin/rails"
       projectile-rails-spring-command "bin/spring"
       projectile-rails-zeus-command "bin/zeus")
+
+(define-key projectile-rails-mode-map (kbd "C-c r") 'projectile-rails-command-map)
