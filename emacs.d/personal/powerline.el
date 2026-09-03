6d95ec0315e37599d1ee122e24f42b5e551d50bc
Ryan McGeary <ryan@mcgeary.org>
2026-09-02 22:07:11 -0600

    Simplify emacs powerline

--- emacs.d/personal/powerline.el
+++ emacs.d/personal/powerline.el
@@ -1,11 +1,11 @@
-(setq powerline-arrow-shape 'arrow)
+;; (setq powerline-arrow-shape 'arrow)
 
 (powerline-default-theme)
 
-(setq powerline-image-apple-rgb nil)
+;; (setq powerline-image-apple-rgb nil)
 
-(custom-set-faces
- '(mode-line         ((t (:background "#ffcc00" :foreground "black"))))
- '(powerline-active1 ((t (:background "grey22" :foreground "white" :inherit mode-line))))
- '(powerline-active2 ((t (:background "grey40" :foreground "white" :inherit mode-line))))
- )
+;; (custom-set-faces
+;;  '(mode-line         ((t (:background "#ffcc00" :foreground "black"))))
+;;  '(powerline-active1 ((t (:background "grey22" :foreground "white" :inherit mode-line))))
+;;  '(powerline-active2 ((t (:background "grey40" :foreground "white" :inherit mode-line))))
+;;  )
