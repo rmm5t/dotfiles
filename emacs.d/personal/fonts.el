e00f3397348f527c1c94355347a94299a8b2e8b6
Ryan McGeary <ryan@mcgeary.org>
2026-09-02 22:07:03 -0600

    Change emacs font

--- emacs.d/personal/fonts.el
+++ emacs.d/personal/fonts.el
@@ -3,7 +3,8 @@
 (when (eq system-type 'darwin)
   ;; default Latin font (e.g. Consolas)
   ;; default font size (point * 10)
-  (set-face-attribute 'default nil :family "Monaco" :height 120 :weight 'normal))
+  ;; (set-face-attribute 'default nil :family "Monaco" :height 120 :weight 'normal))
+  (set-face-attribute 'default nil :family "MesloLGS Nerd Font" :height 120 :weight 'normal))
 
 ;; To change a font size globally:
 ;; (set-face-attribute 'default nil :height 360)
