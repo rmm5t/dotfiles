9c2cb224d219c0bb240af16ae63755fc443eb60b
Ryan McGeary <ryan@mcgeary.org>
2026-09-02 22:22:27 -0600

    Change cmd-k in wezterm to clear scrollback and reset prompt

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config = {
    window_close_confirmation = "NeverPrompt",
@@ -10,5 +11,22 @@ config = {
    color_scheme = "Tokyo Night",
    font = wezterm.font "MesloLGS Nerd Font",
    font_size = 14,
+
+   window_background_opacity = 0.95,
+   macos_window_background_blur = 10,
 }
+
+config.keys = {
+  -- Clears the scrollback and viewport, and then sends CTRL-L to ask the
+  -- shell to redraw its prompt
+  {
+    key = "k",
+    mods = "CMD",
+    action = act.Multiple {
+      act.ClearScrollback "ScrollbackAndViewport",
+      act.SendKey { key = "L", mods = "CTRL" },
+    },
+  },
+}
+
 return config
